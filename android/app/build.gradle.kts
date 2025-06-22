bbfa3d60cde34b380eb28c539ad9fcd2e016ddec
Noam Kontorovich <150962340+NoamKont@users.noreply.github.com>
2025-06-22 13:48:44 +0300

    fixing map API problem

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -28,6 +29,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["MAPS_API_KEY"] = project.findProperty("MAPS_API_KEY") ?: "MISSING_KEY"
     }
 
     buildTypes {
@@ -38,7 +40,6 @@ android {
         }
     }
 }
-
 flutter {
     source = "../.."
 }
